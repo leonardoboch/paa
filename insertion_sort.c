32b6c454848d330d79a43f3b4597e3ebea7c4fe1
Leonardo Bochnia <bochnialeonardo@gmail.com>
2024-05-15 02:32:31 -0300

    Added reverse_insertion_sort function wich orders the array in decreasing order

--- insertion_sort.c
+++ insertion_sort.c
@@ -14,8 +14,25 @@ int* insertion_sort(int arr[], int size) {
     return arr;
 
 }
+
+int* reverse_insertion_sort(int arr[], int size) {
+	for(int j = 1; j < size; j++){
+		int key = arr[j];
+		int i = j - 1;
+
+		while(key > arr[i] && i >= 0){
+			arr[i + 1] = arr[i];
+			i--;
+		}
+		arr[i + 1] = key;
+
+	}
+	return arr;
+}
 int main () {
     int arr[] = {5,2,4,6,1,3};
+    int option;
+    int *tidy;
     printf("Disorderly arrangement: \n");
     printf("{");
     for(int i = 0; i < 6; i++) {
@@ -24,8 +41,19 @@ int main () {
     }
     printf("}");
     printf("\n");
-    
-    int *tidy = insertion_sort(arr, 6);
+
+    printf("1 ---> increasing order \n");
+    printf("2 ---> decresing order \n ");
+    scanf("%d", &option);
+
+    if(option == 1) {
+	    tidy = insertion_sort(arr, 6);
+    }
+    else {
+
+	    tidy = reverse_insertion_sort(arr, 6);
+    }
+
     printf("orderly arrangement: \n");
     printf("{");
     for(int i = 0; i < 6; i++) {
